d401d80274e6707f9479b9f0d7b726fdcf987e83
Gabriel Givigier <gabriel@givigier.com>
2016-03-09 14:59:47 -0300

    Add response status test to UsersController

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe UsersController, type: :controller do
     let!(:user){ create(:user) }
     before(:each){ get :index }
 
+    it "have ok status" do
+      expect(response).to have_http_status(:ok)
+    end
+
     it "return all users" do
       expect(json).to match_array([{ name: user.name }.with_indifferent_access])
     end
