301de6c6be2a5712ab43a9c71cb99ffb9c9295d5
gkunwar <gkunwar09@gmail.com>
2016-03-13 10:11:10 +0545

    added functionality to the shows index action so that it  returns the last show of each user ordered by user's created_at

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,4 +1,7 @@
 class ShowsController < ApplicationController
   def index
+    @shows = Array.new
+    User.order('created_at DESC').includes(:shows).map{|u| @shows << u.shows.last}
+    render json: @shows
   end
 end
